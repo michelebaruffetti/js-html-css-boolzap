1cc0ff2aa965c573cc7473d8612a38ff8f2bf185
michele baruffetti <michelebaruffetti@gmail.com>
2020-05-11 17:55:09 +0200

    Aggiunto riazzeramento della zona testo dopo invio messaggio

--- main.js
+++ main.js
@@ -4,27 +4,32 @@ $(document).ready(function() {
     $('.text-message').keypress(function()
     {
         if (event.which == 13 ) {
-            console.log('hai premuto invio');
+
 
             // leggo il contenuto dell'input text-message e lo salvo in una variabile
             var user_text = $('.text-message').val();
             console.log(user_text);
+
+
 
             // copio l'elemento template in una variabile e tolgo la classe template
             var new_user_message = $('.template').clone().removeClass('template');
             console.log(new_user_message);
 
 
-            // // inserisco il testo copiato dall'input (ma non riesco a inserirlo in un elemento del div copiato)
+            // // inserisco il testo copiato dall'input nel 'p' all'interno del div template precedentemente clonato
             new_user_message.find('p').text(user_text);
-            // console.log(new_user_message);
 
-            // appendo il nuovo messaggio
+
+            // appendo il nuovo messaggio clonato, tolto della classe template e con la variabile copiata nel 'p'
 
             $('.main-talk').append(new_user_message);
 
+            // svuoto text-message (non funziona)
+            $('.text-message').val(" ");
         };
     });
+
 
 
 
